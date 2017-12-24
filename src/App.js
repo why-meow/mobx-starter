66a1f585e294ea58ee4816c5a6fa4c6a79e8cdf7
why-meow <another.quokka@gmail.com>
2017-12-24 22:32:39 +0200

    add mobx and mobx-react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { observer } from 'mobx-react';
 
 import theme from './App.css';
 
+@observer
 class App extends React.Component {
   render() {
+    const { counter } = this.props;
     return (
       <div className={theme.root}>
         <h1 className={theme.header}>
           Hello World!
         </h1>
+        <div>{counter.value}</div>
+        <button onClick={counter.inc.bind(counter)}>+</button>
+        <button onClick={counter.dec.bind(counter)}>-</button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {observable} from 'mobx';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const counter = observable({
+  value: 1,
+  inc() {
+    ++this.value;
+  },
+  dec() {
+    --this.value;
+  }
+});
+
+ReactDOM.render(<App counter={counter}/>, document.getElementById('root'));
